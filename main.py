0948771e72fe25e73f336c5c20217f85bfa296ca
sigmotoa <si.galvismotoa@gmail.com>
2025-03-10 10:45:49 -0500

    Primera version del Home

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, Query
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import numpy as np
+import os
+from typing import List, Optional
+from schemas import (InputModelCircuitDiode,OutputModelCircuitDiode)
+from services.newton_raphson import *
 
-app = FastAPI()
+app = FastAPI(
+    title="NuMaaS - Numerical Methods as a Software",
+    description="the first Numerical Methods as a Software development",
+    version="0.0.1"
+)
 
+templates_dir=os.path.join(os.path.dirname(__file__),"templates")
+templates = Jinja2Templates(directory=templates_dir)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request:Request):
+    return templates.TemplateResponse(
+        "home.html",{"request":request,"title":"NuMaaS - Numerical Methods as a Software"}
+    )
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
